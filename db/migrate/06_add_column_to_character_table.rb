14892f050fc41d1a823ba4528ffe7c182f4bfa3d
Eric <eric.m.lombardo@gmail.com>
2021-01-26 14:55:17 -0500

    Create domain model for TV series.

---
+++ db/migrate/06_add_column_to_character_table.rb
@@ -0,0 +1,6 @@
+class AddColumnToCharacterTable < ActiveRecord::Migration[5.1]
+
+  def change
+    add_column :characters, :catchphrase, :string
+  end
+end
---
+++ db/migrate/07_add_day_column_to_show_table.rb
@@ -0,0 +1,6 @@
+class AddDayColumnToShowTable < ActiveRecord::Migration[5.1]
+  
+  def change
+    add_column :shows, :day, :string
+  end
+end
